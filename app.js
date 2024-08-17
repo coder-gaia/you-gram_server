de51648defecd1122b8a9918eef7b0d173fa6d5a
Alexandre <alexandregaia911@gmail.com>
2024-08-17 20:40:33 -0300

    adding trusted url to cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
     credentials: true,
-    origin: ["http://127.0.0.1:5173"],
+    origin: ["http://127.0.0.1:5173", "https://you-gram-client.onrender.com"],
     optionsSuccessStatus: 200,
   })
 );
