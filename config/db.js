50a2c279098fcda537d41f45dcb80cb7f2813918
Alexandre <alexandregaia911@gmail.com>
2024-08-17 19:07:33 -0300

    adding some stuff

--- config/db.js
+++ config/db.js
@@ -2,11 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
 
 const conn = async () => {
   try {
     const dbConn = await mongoose.connect(
-      `mongodb+srv://alexandre_silva:ZP92zlRCyvL7LncQ@cluster0.t9rgc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.t9rgc.mongodb.net/${dbName}?retryWrites=true&w=majority`
     );
     console.log("Successfully connected to database!");
 
